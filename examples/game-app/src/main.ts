1c0f22d385aaf4e7f07ce9a3c5285b6c8856e0f3
Anatolio ✨ <63070223+ktitaro@users.noreply.github.com>
2024-03-16 03:18:41 +0300

    Update main.ts

--- examples/game-app/src/main.ts
+++ examples/game-app/src/main.ts
@@ -1,16 +1,15 @@
-import 'normalize.css'
-import { HomePage } from './scenes'
-import { Application } from 'pixi.js'
 import { setupDevtools } from '@pixi-devtools/plugin'
+import { Application } from 'pixi.js'
+import { HomePage } from './scenes'
+import 'normalize.css'
 
 // Configures the game app
 const app = new Application()
 await app.init({ resizeTo: window })
 document.body.appendChild(app.canvas)
 setupDevtools(app)
-window.__APP__ = app
-export { app }
 
 // Renders the initial scene
 app.stage.addChild(new HomePage())
 app.renderer.render(app.stage)
+export { app }
